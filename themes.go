0d3285ebbc1892fc650151ecfc21a5788c5f87a1
che1nov <ilya.chernov1@icloud.com>
2026-01-21 15:35:43 +0400

    add weekends

--- themes.go
+++ themes.go
@@ -8,14 +8,21 @@ type Theme struct {
 	Future     color.RGBA
 	Text       color.RGBA
 	Today      color.RGBA
+	Weekend    color.RGBA
 }
 
 var Themes = map[string]Theme{
 	"ios": {
-		Background: color.RGBA{0, 0, 0, 255},       // ЧЁРНЫЙ фон
+		Background: color.RGBA{0, 0, 0, 255},       // фон
 		Active:     color.RGBA{220, 220, 220, 255}, // прошедшие дни
 		Future:     color.RGBA{90, 90, 90, 255},    // будущие дни
 		Text:       color.RGBA{200, 200, 200, 255}, // текст
 		Today:      color.RGBA{255, 140, 0, 255},   // сегодня
+		Weekend:    color.RGBA{120, 120, 120, 255}, // выходные
 	},
+}
+
+// Используется в handler.go
+func IOSTheme() Theme {
+	return Themes["ios"]
 }
